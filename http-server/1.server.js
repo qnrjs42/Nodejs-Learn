a3e5ec152bf4b11a977f78841801c72c4200d665
qnrjs42 <qnrjs42@naver.com>
2020-10-16 02:29:40 +0900

    4-2. fs로 HTML 읽어 제공

--- http-server/1.server.js
+++ http-server/1.server.js
@@ -1,11 +1,17 @@
 const http = require('http');
 
 
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
     res.write('<h1>Hello Node!</h1>');
     res.write("<h1>Hello Server!</h1>");
     res.end("<h1>Hello</h1>");
 })
-.listen(8080, () => {
-    console.log('8080번 포트에서 서버 대기 중');
+.listen(8080);
+
+server.on('listening', () => {
+    console.log("8080번 포트에서 서버 대기 중");
+});
+server.on('error', (err) => {
+    console.error(err);
 })
