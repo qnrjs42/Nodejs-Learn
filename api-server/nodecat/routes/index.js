7480659d0bea2420c67bceec6d314fea9f16c4c6
qnrjs42 <qnrjs42@naver.com>
2020-10-24 18:00:14 +0900

    10-5. NodeBird API 호출하기

--- api-server/nodecat/routes/index.js
+++ api-server/nodecat/routes/index.js
@@ -3,30 +3,56 @@ const axios = require('axios');
 
 const router = express.Router();
 
-router.get('/test', async (req, res, next) => { // 토큰 테스트 라우터
+const URL = 'http://localhost:8002/v1'; // 토큰 발급 URL
+axios.defaults.headers.origin = 'http://localhost:4000'; // origin 헤더 추가
+
+// 토큰 자동 재발급
+const request = async (req, api) => {
   try {
-      console.log('hello?');
     if (!req.session.jwt) { // 세션에 토큰이 없으면 토큰 발급 시도
-      const tokenResult = await axios.post('http://localhost:8002/v1/token', {
+      const tokenResult = await axios.post(`${URL}/token`, {
         clientSecret: process.env.CLIENT_SECRET,
       });
-      if (tokenResult.data && tokenResult.data.code === 200) { // 토큰 발급 성공
-        req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
-      } else { // 토큰 발급 실패
-        return res.json(tokenResult.data); // 발급 실패 사유 응답
-      }
+      // 토큰 발급 성공
+      req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
     }
-    // 발급받은 토큰 테스트
-    const result = await axios.get('http://localhost:8002/v1/test', {
+
+    // 원래 보내고 싶었던 주소로 요청을 보냄
+    return await axios.get(`${URL}${api}`, {
       headers: { authorization: req.session.jwt },
-    });
-    return res.json(result.data);
+    }); // API 요청
+    
+  } catch (error) {
+    if (error.response.status === 419) { // 토큰 만료시 토큰 재발급 받기
+      delete req.session.jwt;
+      // 토큰 만료 되었을 때 토큰 재발급
+      return request(req, api);
+    } // 419 외의 다른 에러면
+    return error.response;
+  }
+};
+
+router.get('/mypost', async (req, res, next) => {
+  try {
+    const result = await request(req, '/posts/my'); // api-server/routes/v1 - /posts/my
+    res.json(result.data);
   } catch (error) {
     console.error(error);
-    if (error.response.status === 419) { // 토큰 만료 시
-      return res.json(error.response.data);
+    next(error);
+  }
+});
+
+router.get('/search/:hashtag', async (req, res, next) => {
+  try {
+    const result = await request(
+      req, `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`, // api-server/routes/v1 - /posts/hashtag/노드
+    );
+    res.json(result.data);
+  } catch (error) {
+    if (error.code) {
+      console.error(error);
+      next(error);
     }
-    return next(error);
   }
 });
 
--- api-server/routes/v1.js
+++ api-server/routes/v1.js
@@ -2,7 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 
 const { verifyToken } = require('./middlewares');
-const { Domain, User } = require('../models');
+const { Domain, User, Post } = require('../models');
 
 const router = express.Router();
 
@@ -47,6 +47,49 @@ router.post('/token', async (req, res) => {
 // 토큰 제대로 발급 됐는지 테스트 라우터
 router.get('/test', verifyToken, (req, res) => {
   res.json(req.decoded);
+});
+
+// nodebird 데이터 보내주는 라우터
+router.get('/posts/my', verifyToken, (req, res) => {
+  Post.findAll({ where: { userId: req.decoded.id } })
+    .then((posts) => {
+      res.json({
+        code: 200,
+        payload: posts,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({
+        code: 500,
+        message: '서버 에러',
+      });
+    });
+});
+
+// 해시태그로 검색하는 라우터
+router.get('/posts/hashtag/:title', verifyToken, async (req, res) => {
+  try {
+    const hashtag = await Hashtag.findOne({ where: { title: req.params.title } });
+
+    if(!hashtag) {
+      return res.status(404).json({
+        code: 404,
+        message: '검색 결과가 없습니다.',
+      });
+    }
+    const posts = await hashtag.getPosts();
+    return res.json({
+      code: 200,
+      payload: posts,
+    });
+  } catch(err) {
+    console.error(err);
+    return res.status(500).json({
+      code: 500,
+      message: '서버 에러',
+    });
+  }
 });
 
 module.exports = router;
