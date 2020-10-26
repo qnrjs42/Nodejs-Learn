65e5fd6b621a731245c6d98139b3869b32a61bf7
qnrjs42 <qnrjs42@naver.com>
2020-10-27 04:52:17 +0900

    15-2. pm2 사용하기

--- prod-sns/config/config.js
+++ prod-sns/config/config.js
@@ -11,7 +11,7 @@ module.exports = {
   test: {
     username: "root",
     password: process.env.SEQUELIZE_PASSWORD,
-    database: "nodejsbook-nodebird_test",
+    database: "nodejsbook-nodebird-test",
     host: "127.0.0.1",
     dialect: "mysql",
   },
